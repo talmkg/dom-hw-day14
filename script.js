25f513723ab613f7d883ff829a9cbdc1ef9592af
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-10-15 15:54:19 +0200

    fixes

--- script.js
+++ script.js
@@ -2,26 +2,35 @@ const createCells = function () {
   const bingotable = document.getElementById("bingotable");
 
   for (let i = 0; i < 76; i++) {
-    const bingoCellNode = document.createElement("div"); // this will create an empty cell
-    bingoCellNode.className = "cell_class"; // assigning the CSS class
+    const bingoCellNodes = document.createElement("div"); // this will create an empty cell
+    bingoCellNodes.className = "cell_class"; // assigning the CSS class
 
     const h3 = document.createElement("h3"); // label for day's number
     // adding the number as innerText of the day's <h3> we've created
     h3.innerText = i + 1; // the for loop gives a 0 based index, let's increase it by 1 to count days properly
 
-    bingoCellNode.appendChild(h3); // <div class="day"><h3 /></div>
-    bingotable.appendChild(bingoCellNode); // <div id="calendar"> <div class="day"><h3 /></div> </div>
+    bingoCellNodes.appendChild(h3); // <div class="day"><h3 /></div>
+    bingotable.appendChild(bingoCellNodes); // <div id="calendar"> <div class="day"><h3 /></div> </div>
   }
 };
 window.onload = function () {
   createCells(76);
 };
 
-function getRndInteger() {
+const giveRandomNumber = () => {
   return Math.floor(Math.random() * 76);
-}
-if (getRndInteger() == bingoCellNode) {
-  for (let bingoCellNodes of bingoCellNode) {
-    bingoCellNodes.style.color = "white";
+};
+
+function compareNumbers() {
+  const num = giveRandomNumber();
+
+  for (bingoCellNode of bingoCellNodes) {
+    if (bingoCellNode.innerText == num) {
+      //apply style
+      bingoCellNode.style.backgroundColor = "green";
+    }
   }
 }
+const buttoncall = document
+  .getElementById("button")
+  .addEventListener("click", compareNumbers);
